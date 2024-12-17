4a6d6874c7e86b93e496fa871ae5801d2ae3a8eb
ThomRoman <thom.maurick.r@gmail.com>
2024-12-17 12:36:15 -0500

    otros cambios

--- init.sh
+++ init.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+PIDS=$(ps aux | grep 'java' | awk '{print $1}')
+
+if [ -n "$PIDS" ]; then
+  for PID in $PIDS; do
+    echo "Deteniendo el proceso con PID: $PID"
+    kill $PID
+  done
+else
+  echo "No se encontraron procesos java -jar en ejecución."
+fi
+
+
 BASE_DIR="$(pwd)"
 
 LOG_DIR="$BASE_DIR/logs"
 
 mkdir -p "$LOG_DIR"
-
 
 
 ### CONFIG SERVER
